ee37179f516f43e88fa64c529fcbc879893a3113
mda1422 <maxamedmustafe2384@gmail.com>
2024-03-22 13:24:05 +0000

    22/3/24

---
+++ flickr.php
@@ -0,0 +1,28 @@
+<?php   
+
+echo '<link rel="stylesheet" href="flickr.css">';
+
+$tag = 'londonbuildings,londonnight,londonpeople,londonarchitecture'; // Specify the tag here
+$url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=c6fe1464a578e7126b3140f055d2e597&tags='.$tag.'&format=json&nojsoncallback=1';
+
+$data = json_decode(file_get_contents($url));
+
+if ($data && $data->stat == 'ok') {
+    $photos = $data->photos->photo;
+
+    // Output the gallery container
+    echo '<div class="gallery">';
+    
+    foreach ($photos as $photo) {
+        $photo_url = 'http://farm'.$photo->farm.'.staticflickr.com/'.$photo->server.'/'.$photo->id.'_'.$photo->secret.'.jpg';
+        echo '<div class="image-container">';
+        echo '<img src="'.$photo_url.'" class="flickr-image">';
+        echo '</div>';
+    }
+
+    // Close the gallery container
+    echo '</div>';
+} else {
+    echo 'Failed to fetch images from Flickr.';
+}
+?>
