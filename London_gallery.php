97dc0be93ccfe955888911e1c32899542636f570
mda1422 <maxamedmustafe2384@gmail.com>
2024-03-31 18:37:52 +0100

    update on 31/03/24

--- London_gallery.php
+++ London_gallery.php
@@ -1,6 +1,7 @@
 <?php   
-
-echo '<link rel="stylesheet" href="flickr.css">';
+echo '<head><link rel="stylesheet" href="flickr_c.css"><style>body {
+    background-color: #333; /* Dark background color */
+    }</style></head>';
 
 $tag = 'londonbuildings,londonnight,londonpeople,londonarchitecture'; // Specify the tag here
 $url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=c6fe1464a578e7126b3140f055d2e597&tags='.$tag.'&format=json&nojsoncallback=1';
